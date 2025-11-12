db1a1c027df68a5b265196173eae841430426eb2
Fazal <fazalbkabeer@gmail.com>
2025-11-12 22:09:27 +0530

    linkedlist--Traverse,adding to end and beginning

--- datastructure/linkedlist/single.go
+++ datastructure/linkedlist/single.go
@@ -2,28 +2,59 @@ package main
 
 import "fmt"
 
-//structure of a Node: a single element in the linked list
 type Node struct {
 	data int
 	next *Node
 }
+
 type LinkedList struct {
 	head *Node
 }
 
-// AddToFront adds a new node at the beginning of the list
 func (list *LinkedList) AddToFront(data int) {
-	newNode := &Node{data: data} // creating new Node
-	newNode.next = list.head     // point newNode.next to current head
-	list.head = newNode          // move head to point to newNode
+	newNode := &Node{data: data}
+	newNode.next = list.head
+	list.head = newNode
+}
+
+func (list *LinkedList) AddToEnd(data int) {
+	newNode := &Node{data: data}
+
+	if list.head == nil {
+		list.head = newNode
+		return
+	}
+
+	current := list.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = newNode
+}
+
+func (list *LinkedList) Display() {
+	if list.head == nil {
+		fmt.Println("list is empty")
+	}
+
+	current := list.head
+
+	for current != nil {
+		fmt.Println(current.data)
+		current = current.next
+	}
+
+	fmt.Println("nil")
 }
 
 func main() {
-
 	list := LinkedList{}
 
-	list.AddToFront(50)
+	list.AddToEnd(10)
+	list.AddToEnd(50)
+	list.AddToEnd(60)
+	list.AddToFront(70)
 
-	fmt.Println("Added first node with value:", list.head.data)
-	fmt.Println("next of head:", list.head.next)
+	list.Display()
 }
